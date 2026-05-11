9747ab39839a5a780e99c4bcdea19c3cdc639c29
Alessandro <graphic.alessandro@gmail.com>
2026-05-11 11:16:43 +0200

    delate searchBarmenu

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -1,7 +1,6 @@
 import { Link } from 'react-router-dom';
 import '../style/Nav.scss'
 import SearchBar from "./SearchBar";
-import SearchBarMenu from "./SearchBarmenu";
 import vegan from "../img/logo-xs.svg"
 
 
